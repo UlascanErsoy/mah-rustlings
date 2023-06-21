ef4d10072f23b895e497e890ff27c00b267a6404
Ulascan Ersoy <cake@pop-os.localdomain>
2023-06-21 17:43:43 -0400

    Done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,11 +35,37 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+
+        if s.len() == 0 {
+            return Person::default();
+        }else{
+            let params = s.split(",").collect::<Vec<_>>();
+            if params.len() != 2 {
+                return Person::default();
+            }
+            let name = String::from(params[0].trim());
+
+            if name.len() == 0{
+               return Person::default(); 
+            }
+
+            let age = params[1].parse::<usize>();
+
+            match age {
+                Ok(i) => {
+                    return Person { name, age:i }
+                },
+                Err(_) => {
+                    return Person::default()
+                }
+            }
+        }
     }
+
+
 }
 
 fn main() {
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,31 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let params = s.split(",").collect::<Vec<_>>();
+
+        if params.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = String::from(params[0].trim());
+
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age = params[1].parse::<usize>();
+
+        return match age {
+            Ok(i) => Ok(Person { name, age:i }),
+            Err(e) => Err(ParsePersonError::ParseInt(e))
+        };
+        
+        
+
     }
 }
 
